536eb92078e51bec81b36c925f3ce293ba668290
Serhii Melnyk <serhi.melnik@gmail.com>
2024-01-18 07:33:35 -0500

    chore(release): 0.0.3

--- README.md
+++ README.md
@@ -88,21 +88,7 @@ Example of `index.js` handler for actual bot Lambda:
 import { Bot, Handler } from "nvsbot-beta";
 
 export const handler = Handler.createMainLambda(async (message) => {
-  const bot = Bot.createBot({
-    command: "Your command, eg. /bot",
-    openAIKey: "Your openAI key",
-    dynamoDBTableName: "Your DB table name",
-    allowedChats: "List of allowed chat ids, eg. [123,456]",
-    telegramToken: "Your telegram token",
-    defaultResponse: "Default response for /help command",
-    model: "Your GPT model, eg. gpt-4",
-    systemPromptFunc: (username) => {
-      return `
-         Your default system prompt to configure GPT model.
-         After conversation, bot must return JSON object which will indicate end of conversation.`;
-    },
-    telegramBotOptions: {}, // Telegram Bot options
-  });
+  const bot = Bot.createBot(/* configuration */);
   await bot.processMessage(message, async (result, meta) => {
     const telegramBot = bot.getTelegramBot();
     console.log("Result:", result);
--- src/services/state/LocalStateService.ts
+++ src/services/state/LocalStateService.ts
@@ -1,11 +1,17 @@
 import { StateServiceInterface } from "./StateServiceInterface";
 import OpenAI from "openai";
 
+/**
+ * Represents the local state of the application.
+ */
 export type LocalState = {
   id: string;
   messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[];
 }[];
 
+/**
+ * Represents a service for managing local state.
+ */
 export class LocalStateService
   implements
     StateServiceInterface<OpenAI.Chat.Completions.ChatCompletionMessageParam>
@@ -16,12 +22,25 @@ export class LocalStateService
     return this;
   }
 
+  /**
+   * Retrieves an item from the local state by its ID.
+   * @param id - The ID of the item to retrieve.
+   * @returns A promise that resolves to an array of chat completion message parameters.
+   */
   public async getItemById(
     id: string
   ): Promise<OpenAI.Chat.Completions.ChatCompletionMessageParam[]> {
     return this.localState.find((state) => state.id === id)?.messages || [];
   }
 
+  /**
+   * Sets an item in the local state by its ID.
+   * If the item already exists, it updates the messages.
+   * If the item does not exist, it creates a new item with the specified ID and messages.
+   * @param id - The ID of the item to set.
+   * @param update - The updated array of chat completion message parameters.
+   * @returns A promise that resolves when the item is set.
+   */
   public async setItemById(
     id: string,
     update: OpenAI.Chat.Completions.ChatCompletionMessageParam[]
